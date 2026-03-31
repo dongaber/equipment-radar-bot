b0547c842450c5f1fe6c0367dfe09f3f8eb84358
dongaber <boozy044@gmail.com>
2026-04-01 01:19:54 +0300

    feat(constants): add constants for action names

---
+++ src/constants/actions.ts
@@ -0,0 +1,14 @@
+export enum CallbackActions {
+	CommandStart = "command:start",
+	CommandAdmin = "command:admin",
+
+	ConsentApprove = "consent:approve",
+	ConsentReject = "consent:reject",
+	MainMenuChooseMachinery = "main_menu:choose_machinery",
+	MainMenuInsertOrder = "main_menu:insert_order",
+	MainMenuChooseDistrict = "main_menu:choose_district",
+	MainMenuContactUs = "main_menu:contact_us",
+	MainMenuPaySubscription = "main_menu:pay_subscription",
+	MainMenuMyProfile = "main_menu:my_profile",
+	MainMenuBackToMainMenu = "main_menu:back_to_main_menu"
+}
